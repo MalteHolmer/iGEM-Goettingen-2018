7571696e15849732acb95a7a02b1470d83ede509
malte.holmer <malte.holmer@stud.uni-goettingen.de>
2018-07-23 10:52:46 +0200

    bug introduced --> padding on the sides, otherwise added the hidden menu

--- iGEM-Rework.js
+++ iGEM-Rework.js
@@ -1,24 +1,34 @@
 $(document)
-    .ready(function () {
-        $(".dropdown_button")
-            .click(function () {
-                if($(this)
-                    .hasClass("is-sidebar-active")) {
-                    $(this)
-                        .next()
-                        .slideUp(1000);
-                    $(this)
-                        .removeClass("is-sidebar-active");
-                } else {
-                    $(".is-sidebar-active")
-                        .next();
-                    $(".is-sidebar-active")
-                        .removeClass("is-sidebar-active");
-                    $(this)
-                        .next()
-                        .slideDown(1000);
-                    $(this)
-                        .addClass("is-sidebar-active");
-                }
-            });
+  .ready(function () {
+    $(".dropdown_button")
+      .click(function () {
+        if ($(this)
+          .hasClass("is-sidebar-active")) {
+          $(this)
+            .next()
+            .slideUp(1000);
+          $(this)
+            .removeClass("is-sidebar-active");
+        } else {
+          $(".is-sidebar-active")
+            .next();
+          $(".is-sidebar-active")
+            .removeClass("is-sidebar-active");
+          $(this)
+            .next()
+            .slideDown(1000);
+          $(this)
+            .addClass("is-sidebar-active");
+        }
+      });
+
+    $("#show_nav").click(function () {
+      $(this).fadeOut(1000);
+      $("#navigation").fadeIn(1000);
     });
+
+    $("#close_nav").click(function () {
+      $("#navigation").fadeOut(1000);
+      $("#show_nav").fadeIn(1000);
+    })
+  });
